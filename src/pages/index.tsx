2aa3817babfeacb72e9f6f4d360ee0f185699947
gustavorsan <gustavorsantos42@gmail.com>
2022-04-17 14:26:18 -0300

    Adicionado map dos posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
 import { GetStaticProps } from 'next';
+import { format } from 'date-fns';
+
 import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -26,45 +28,31 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   return (
     <>
       <main className={commonStyles.container}>
         <Header />
         <div className={styles.posts}>
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>O Titulo</strong>
-              <p>Texto do post aleatorio para teste de texto</p>
-              <ul>
-                <li>
-                  <AiOutlineCalendar />
-                  <time>10-05-2020</time>
-                </li>
-                <li>
-                  <AiOutlineUser />
-                  <span>José</span>
-                </li>
-              </ul>
-            </a>
-          </Link>
+          {postsPagination.results.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <AiOutlineCalendar />
+                    <time>{post.first_publication_date}</time>
+                  </li>
+                  <li>
+                    <AiOutlineUser />
+                    <span>{post.data.author}</span>
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
 
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>O Titulo</strong>
-              <p>Texto do post aleatorio para teste de texto</p>
-              <ul>
-                <li>
-                  <AiOutlineCalendar />
-                  <time>10-05-2020</time>
-                </li>
-                <li>
-                  <AiOutlineUser />
-                  <span>José</span>
-                </li>
-              </ul>
-            </a>
-          </Link>
           <button type="button">Carregar mais posts</button>
         </div>
       </main>
@@ -74,21 +62,31 @@ export default function Home(): JSX.Element {
 
 export const getStaticProps = async () => {
   const prismic = getPrismicClient();
-  /* const postsResponse = await prismic.get(
-    {
-      predicates: prismic.predicate.at('document.type', 'Publication'),
-    },
-    {
-      fetch: ['Publication.title', 'Publication.content'],
-    }
-  ); */
 
   const postsResponse = await prismic.getAllByType('Publication', {
-    pageSize: 100,
+    pageSize: 1,
+  });
+  console.log(postsResponse);
+  const posts = postsResponse?.map((post: any) => {
+    return {
+      slug: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy'
+      ),
+      data: {
+        author: post.data.author,
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+      },
+    };
   });
 
-  console.log(postsResponse);
+  const postsPagination = {
+    next_page: postsResponse?.next_page ?? null,
+    results: posts,
+  };
   return {
-    props: {},
+    props: { postsPagination },
   };
 };
