9f06c1a7e59ada0efa89c3a521e448c3519651e2
gustavorsan <gustavorsantos42@gmail.com>
2022-04-17 22:11:04 -0300

    Implementada paginação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
 import { GetStaticProps } from 'next';
 import { format } from 'date-fns';
 
+import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -29,14 +30,50 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  async function handleNextPage(): Promise<void> {
+    if (currentPage !== 1 && nextPage === null) {
+      return;
+    }
+    const postsResponse = await fetch(`${nextPage}`).then(response => {
+      return response.json();
+    });
+
+    console.log('posts', postsResponse);
+
+    setNextPage(postsResponse.next_page);
+    setCurrentPage(postsResponse.page);
+
+    const newPosts = postsResponse?.results.map((post: any) => {
+      return {
+        uid: post.uid,
+        slug: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy'
+        ),
+        data: {
+          author: post.data.author,
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+        },
+      };
+    });
+
+    setPosts([...posts, ...newPosts]);
+  }
+
   return (
     <>
       <main className={commonStyles.container}>
         <Header />
         <div className={styles.posts}>
-          {postsPagination.results.map(post => (
+          {posts.map(post => (
             <Link href={`/post/${post.uid}`} key={post.uid}>
-              <a className={styles.post}>
+              <a className={styles.post} key={post.uid}>
                 <strong>{post.data.title}</strong>
                 <p>{post.data.subtitle}</p>
                 <ul>
@@ -53,22 +90,27 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
             </Link>
           ))}
 
-          <button type="button">Carregar mais posts</button>
+          {nextPage && (
+            <button type="button" onClick={handleNextPage}>
+              Carregar mais posts
+            </button>
+          )}
         </div>
       </main>
     </>
   );
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps = async (): Promise<any> => {
   const prismic = getPrismicClient();
 
-  const postsResponse = await prismic.getAllByType('Publication', {
+  const postsResponse = await prismic.getByType('Publication', {
     pageSize: 1,
   });
-  console.log(postsResponse);
-  const posts = postsResponse?.map((post: any) => {
+
+  const posts = postsResponse?.results.map((post: any) => {
     return {
+      uid: post.uid,
       slug: post.uid,
       first_publication_date: format(
         new Date(post.first_publication_date),
